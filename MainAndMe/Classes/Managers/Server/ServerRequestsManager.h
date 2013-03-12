a77d7ac4fa6c4f8e747871c5f2df9281186895e9
Uniprog <alex_unidev@yahoo.com>
2013-03-12 15:13:55 -0300

    Added server manager;

---
+++ MainAndMe/Classes/Managers/Server/ServerRequestsManager.h
@@ -0,0 +1,14 @@
+//
+//  ServerRequestsManager.h
+//  MainAndMe
+//
+//  Created by Sasha on 3/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+
+@interface ServerRequestsManager : NSObject
+
++ (ServerRequestsManager*)shared;
+
+@end
---
+++ MainAndMe/Classes/Managers/Server/ServerRequestsManager.m
@@ -0,0 +1,42 @@
+//
+//  ServerRequestsManager.m
+//  MainAndMe
+//
+//  Created by Sasha on 3/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "ServerRequestsManager.h"
+
+
+@interface ServerRequestsManager()
+
+@end
+
+
+@implementation ServerRequestsManager
+
+#pragma mark - Shared Instance and Init
++ (ServerRequestsManager *)shared {
+    
+    static ServerRequestsManager *shared = nil;
+    static dispatch_once_t oncePredicate;
+    
+    dispatch_once(&oncePredicate, ^{
+        shared = [[self alloc] init];
+    });
+    
+    return shared;
+}
+
+-(id)init{
+    
+    self = [super init];
+    if (self) {
+   		// your initialization here
+    }
+    return self;
+}
+
+#pragma mark - 
+@end
