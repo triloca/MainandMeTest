53646d8c87efc2088bd6843ca1c9f47dce7e84be
Uniprog <alex_unidev@yahoo.com>
2013-03-23 17:02:12 -0300

    Added Address controller;

---
+++ MainAndMe/Classes/Controllers/Address/Views/AddressCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchCell.h
+//  MainAndMe
+//
+//  Created by Sasha on 3/19/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+
+@interface AddressCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+@end
