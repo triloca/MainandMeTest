3f3421bfb8f94f60b59b40a4602bfc53b36b81e1
Uniprog <alex_unidev@yahoo.com>
2013-03-26 19:52:52 -0300

    Added WishlistDetails controller;

---
+++ MainAndMe/Classes/Controllers/WishlistDetails/WishlistDetailsViewController.h
@@ -0,0 +1,15 @@
+//
+//  SearchDetailsViewController.h
+//  MainAndMe
+//
+//  Created by Sasha on 3/19/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WishlistDetailsViewController : UIViewController
+@property (assign, nonatomic) BOOL isNeedRefresh;
+@property (strong, nonatomic) NSDictionary* wishlistInfo;
+@property (assign, nonatomic) BOOL isAllState;
+@end
