efff1d2b3e9cca5e6f291d4f38118feaa2dd7604
Uniprog <alex_unidev@yahoo.com>
2013-04-18 17:01:01 +0300

    Added Community Cell;

---
+++ MainAndMe/Classes/Controllers/Community/Views/CommunityCell.h
@@ -0,0 +1,14 @@
+//
+//  SearchCell.h
+//  MainAndMe
+//
+//  Created by Sasha on 3/19/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+
+@interface CommunityCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+
+@end
