e480da773eeeb2ddc47ba39cd438a0d7bfa3f347
Alexander Bukov <alex_unidev@yahoo.com>
2014-11-09 01:13:31 -0300

    Minor changes;

---
+++ MainAndMeios7/MainAndMeios7/Classes/Controllers/Home/Views/HomeCell.h
@@ -0,0 +1,25 @@
+//
+//  HomeCell.h
+//  MainAndMeios7
+//
+//  Created by Alexander Bukov on 11/3/14.
+//  Copyright (c) 2014 Uniprog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TMQuiltViewCell.h"
+
+@interface HomeCell : TMQuiltViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *mainImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+
+@property (weak, nonatomic) IBOutlet UILabel *descrLabel;
+@property (weak, nonatomic) IBOutlet UILabel *itemNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
+
+@property (strong, nonatomic) NSDictionary* storeDict;
+
+
++ (CGFloat)cellHeghtForStore:(NSDictionary*)storeDict;
+
+@end
